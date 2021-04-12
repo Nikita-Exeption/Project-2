4d8bc30aefce3a9b668865a7b21812e8893a5c12
Nikita Gladkov <77583149+Nikita-Exeption@users.noreply.github.com>
2021-04-12 15:20:50 +0300

    ver.3

--- src/org/Nikita/Main.java
+++ src/org/Nikita/Main.java
@@ -1,31 +1,42 @@
 package org.Nikita;
 
-import java.time.DateTimeException;
+
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.Scanner;
 
 public class Main {
 
+    public final static int AGE_OF_MAJORITY = 18;
+
     public static void main(String[] args) {
-        try {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Введите день своего рождения");
-            int day = scanner.nextInt();
-            System.out.println("Введите месяць своего рождения");
-            int month = scanner.nextInt();
-            System.out.println("Введите год своего рождения");
-            int years = scanner.nextInt();
-            LocalDate birthday = LocalDate.of(years, month, day);
-            Period period_18 = Period.between(birthday, LocalDate.now());
-            if (period_18.getYears() >= 18) {
-                System.out.println("Вам есть 18!");
-            } else {
-                System.out.println("Вам нет 18!");
-            }
-        } catch (DateTimeException exception) {
-            System.out.println("Вы ввели неправильные данные");
-            System.out.print(exception.getMessage());
+        LocalDate birthday = getUserBirthday();
+        if (isAdult(birthday)) ;
+
+
+    }
+
+    private static boolean isAdult(LocalDate birthday) {
+        Period fullAge = Period.between(birthday, LocalDate.now());
+        if (fullAge.getYears() >= AGE_OF_MAJORITY) {
+            System.out.println("Вам есть 18! Ура");
+            return true;
+        } else {
+            System.out.println("Вам нет 18 :(");
+            return false;
         }
     }
+
+    private static LocalDate getUserBirthday() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число рождения:");
+        int dayOfMonth = scanner.nextInt();
+        System.out.println("Введите месяць рождения:");
+        int month = scanner.nextInt();
+        System.out.println("Введите год рождения:");
+        int years = scanner.nextInt();
+        LocalDate birthday = LocalDate.of(years, month, dayOfMonth);
+        return birthday;
+    }
+
 }
